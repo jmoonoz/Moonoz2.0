f03a366a6bdf0ccfadb6f7618b4044a46c667bd1
Joel Munoz <j.munoz1@yahoo.com>
2021-10-28 22:38:56 -0700

    About section component created

---
+++ src/components/About.js
@@ -0,0 +1,22 @@
+import { Container, Col, Row } from 'react-bootstrap';
+
+function About() {
+    return (
+        <Container fluid>
+            <Container>
+            <Row>
+                <Col sm={3}>
+
+                </Col>
+                <Col sm={8}>
+
+                </Col>
+            </Row>
+
+            </Container>
+        </Container>
+
+    )
+}
+
+export default About;
