c5dca4a97a4188c1679bcfc5563701e894a39413
Joel Munoz <j.munoz1@yahoo.com>
2021-11-17 18:23:18 -0800

    masonry adjustment

--- src/components/Design.js
+++ src/components/Design.js
@@ -1,4 +1,25 @@
-import { Container, Col, Image, Row } from 'react-bootstrap';
+import { Container, Col, Row } from 'react-bootstrap';
+import Masonry from 'masonry-layout';
+import imagesLoaded from 'imagesloaded';
+import Img1 from '../img/01.1.jpg';
+import Img2 from '../img/01.2.jpg';
+import '../style/style.scss';
+
+window.onload  = () => {
+    const grid = document.querySelector('grid');
+
+    const masonry = new Masonry(grid, {
+        itemSelector: ".grid-item",
+        columnWidth: ".grid-sizer",
+        percentPosition: true
+    });
+
+    imagesLoaded(grid).on('progress', function () {
+        // layout Masonry after each image loads
+        masonry.layout();
+    });
+};
+
 
 
 function Design() {
@@ -10,35 +31,25 @@ function Design() {
                         <h2>
                             Design
                         </h2>
-                        <p>sjf laksjdf;lk </p>
+                        <p>Take a look at the mind of Moonoz and see my thoughts come to life!</p>
 
                     </Col>
-                    <Col className='right-col' sm='8'>
-                        <Row className='inline-block'>
-                            <Col sm='8'>
-                                <div class='design'>
-                                    <div class='design-overlay'>
-                                        <img class="full-width img-responsive" src="img/397x300/03.jpg" alt="Portfolio Image" />
-                                    </div>
-                                </div>
-
-                            </Col>
-                            <Col sm='3'>
-                                <Image className='img-responsive' src={} />
-
-                            </Col>
-                        </Row>
-                        <Row>
-
-                        </Row>
-
+                    <Col className='grid' sm={9}>
+                        <div class="grid-sizer col-xs-6 col-sm-6 col-md-1"></div>
+                        <div class="grid-item col-xs-12 col-sm-6 col-md-8">
+                            <img class="full-width" src={Img1}></img>
+                        </div>
+                        <div class="grid-item col-xs-12 col-sm-6 col-md-4">
+                            <img class='full-width' src={Img2}></img>
+                        </div>
                     </Col>
                 </Row>
-
             </Container>
         </Container>
 
     )
 }
+
+
 
 export default Design;
---
+++ src/components/Masonry.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import Masonry from 'masonry-layout';
+import imagesLoaded from 'imagesloaded';
+
