172bd4715fff082800585465a89b74e652163ec0
Joel Munoz <j.munoz1@yahoo.com>
2021-12-09 18:53:15 -0800

    build update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Home from './components/Home';
-import Nav from './components/NavBar.js';
+import Nav from './components/NavBar';
 import Footer from './components/Footer';
 import Contact from './components/Contact';
 import Design from './components/Design';
